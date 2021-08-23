f49df3c075adbd5fb1f450a2eb537bc432b1d905
codeblizz <codeblizzen@gmail.com>
2021-08-23 05:43:04 +0400

    Update NavBar.jsx

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -37,7 +37,7 @@ function Navbar(props) {
               return (
                 <li key={index} className={item.cName} onClick={() => navClick(item)}>
                   <Link to={item.path} className="nav-text">
-                    {item.icon}
+                    <span>{item.icon}</span>
                     <span>{item.title}</span>
                   </Link>
                 </li>
